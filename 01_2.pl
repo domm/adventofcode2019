db6b464b7baf855160e92960ffb2e95f5fd6a539
Thomas Klausner <domm@plix.at>
2019-12-01 14:12:24 +0100

    clean up day 1 task 2

--- 01_2.pl
+++ 01_2.pl
@@ -1,21 +1,12 @@
-
 use 5.010;
 
-while (<STDIN>) { chomp; 
-    #say "module mass $_"; 
-    my $f = fuel($_); 
-    #say "module fuel: $f"; 
-    $a+=$f;
-}; print $a;
+while (<STDIN>) { $a+=fuel($_) }; say $a;
 
 sub fuel {
     my $mass = shift;
     my $fuel = int($mass/3)-2;
-    $fuel = 0 if $fuel < 0;
-    #say "$mass -> $fuel";
-    if ($fuel > 0) {
-        $fuel += fuel($fuel);
-    }
+    return 0 if $fuel <= 0;
+    $fuel += fuel($fuel);
     return $fuel;
 }
 
