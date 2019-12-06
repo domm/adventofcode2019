c220e46b3653c7697eb82d6957c2a8986916f9ef
Thomas Klausner <domm@plix.at>
2019-12-06 07:56:29 +0100

    clean up 06_2

--- 06_2.pl
+++ 06_2.pl
@@ -7,36 +7,23 @@ my %o;
 for (<STDIN>) {
     chomp();
     my ($a, $b) = split(/\)/,$_);
-
-    say "$b orbits $a";
     $o{$b} = $a;
 };
 
-my %you;
-my %santa;
-transfer(\%you, $o{YOU}, 0,0);
-my $common = transfer(\%santa, $o{SAN}, 0,1);
+my %map;
+transfer('YOU');
+my $common = transfer('SAN');
 
-say $you{$common} + $santa{$common};
-
+say $map{YOU}->{$common} + $map{SAN}->{$common};
 
 sub transfer {
-    my ($col, $ob, $cnt, $is_santa) = @_;
+    my ($who) = @_;
+    my $ob = $o{$who};
     while (1) {
-        return unless $ob;
-        #say "ob $ob";
-        if (my $p = $o{$ob}) {
-            #   say "orbits $p";
-            $cnt++;
-            $col->{$p} = $cnt;
-            if ($is_santa) {
-                return $p if $you{$p};
-            }
-            $ob = $p;
-        }
-        else { return }
+        return unless my $p = $o{$ob};
+        $map{$who}->{$p} = ++$map{cnt}->{$who};
+        return $p if $who eq 'SAN' && $map{YOU}->{$p};
+        $ob = $p;
     }
 }
-
-
 
