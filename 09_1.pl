1cc4c3cd998e1af62fdfd8324b5fc27ff55c3c16
Thomas Klausner <domm@plix.at>
2019-12-09 08:31:00 +0100

    day 9

---
+++ 09_1.pl
@@ -0,0 +1,20 @@
+use 5.030;
+use strict;
+use warnings;
+use lib '.';
+use Intcode;
+my @code;
+{
+    open my $fh, '<', $ARGV[0] or die;
+    local $/ = undef;
+    @code = split( ',', <$fh> );
+}
+
+my $intcode = Intcode->new( [@code] );
+$intcode->input( [2 ]);
+while (!$intcode->halted) {
+    $intcode->runit;
+    my $output = $intcode->output;
+    say "o ".$output;
+}
+
