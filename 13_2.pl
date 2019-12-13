2a49d8b11d8c73766481836850a135f656d3dbd0
Thomas Klausner <domm@plix.at>
2019-12-13 11:23:39 +0100

    autoplay sort of works, but loses after some time :-(

--- 13_2.pl
+++ 13_2.pl
@@ -5,16 +5,12 @@ use lib '.';
 use Intcode;
 
 my $intcode = Intcode->from_file($ARGV[0]);
-#$intcode->input( [ 2 ]);
 $intcode->code->[0]=2;
 my @data;
 while (!$intcode->waiting) {
     $intcode->runit;
     push(@data,$intcode->output);
 }
-
-#@data = (1,2,3,6,5,4);
-
 my @game;
 my $clear_string = `clear`;
 
@@ -30,43 +26,19 @@ while (@data) {
         $ball=$x;
     }
 }
-my %in=(
-j=>-1,
-k=>0,
-l=>1
-);
 my @score;
 my $total=0;
 say "$ball $paddle";
-my $ball_dir=0;
-my $no_first_left=1;
+my $move = 0;
 show();
+$intcode->input([0]);
 while (!$intcode->halted) {
-    my $x;
     say "Ball $ball paddle $paddle";
-    if ($intcode->waiting) {
-        my $in=0;
-        say "Ball $ball paddle $paddle";
-        if ($ball > $paddle) {
-            $in=1;
-            say "move right";
-        }
-        elsif ($ball < $paddle) {
-            $in = -1;
-            if ($no_first_left) {
-                $in=0;
-                $no_first_left=0;
-            }
-            say "move left";
-        }
-        else {
-            #      $in = $ball_dir;
-        }
-        say "in $in";
-        $intcode->input([$in]);
-        select(undef,undef,undef,0.1);
-        $intcode->waiting(0);
-    }
+    #if ($intcode->waiting) {
+    #    $intcode->input([$move]);
+    #    select(undef,undef,undef,0.0005);
+    #    $intcode->waiting(0);
+    #}
     my @update;
     for (1..3) {
         $intcode->runit;
@@ -82,21 +54,33 @@ while (!$intcode->halted) {
         $game[$update[1]]->[$update[0]] = $update[2];
         if ($update[2] == 4) {
             if ($update[0] !=4 && $update[1] !=4) {
-                say "ball moved from $ball to $update[0]";
-                $ball_dir = $update[0] > $ball ? 1 : -1;
+                say "paddle at $paddle, ball moved from $ball to $update[0]";
                 $ball = $update[0];
+                if ($ball > $paddle) {
+                    $move=1;
+                }
+                elsif ($ball < $paddle) {
+                    $move=-1;
+                }
+                else {
+                    $move = 0;
+                }
+                say "paddle move: $move";
             }
         }
-        elsif ($update[2] = 3) {
+        elsif ($update[2] == 3) {
+            say "paddled moved from $paddle to ".$update[0];
             $paddle = $update[0];
         }
     }
+    $intcode->input([$move]);
+        select(undef,undef,undef,0.0005);
     show();
 }
 
 # 45,75, 166,208
 sub show {
-    #print $clear_string;
+    print $clear_string;
     say "j=left, k=stay, l=right    SCORE ".$total;
     for my $r ( @game ) {
         for my $c ( @$r ) {
