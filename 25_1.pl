ab5afe0038a9c8f74a3eafef4e8d6aa26e0b9898
Thomas Klausner <domm@plix.at>
2019-12-26 12:24:18 +0100

    use ascii helpers, cleanup day 25

--- 25_1.pl
+++ 25_1.pl
@@ -5,129 +5,75 @@ use lib '.';
 use Intcode;
 use Math::Combinatorics;
 
-my $intcode = Intcode->from_file($ARGV[0]);
+my $intcode = Intcode->from_file( $ARGV[0] );
 
-my @collect=(
-'south',
-'take fixed point',
-'north',
-'west',
-'west',
-'west',
-'take hologram',
-'east',
-'east',
-'east',
-'north',
-'take candy cane',
-'west',
-'take antenna',
-'west',
-'take shell',
-'east',
-'south',
-'take whirled peas',
-'north',
-'east',
-'north',
-'north',
-'take polygon',
-'south',
-'west',
-'take fuel cell',
-'west',
-'west',
-);
-
-
-my @things=(
-'hologram',
-'shell',
-'whirled peas',
-'fuel cell',
-'fixed point',
-'polygon',
-'antenna',
-'candy cane',
-);
-
-my $newline=0;
-while (@collect) {
-    input(shift(@collect));
-    doit();
+if ($ARGV[1] eq 'manual') {
+    $intcode->ascii_run;
+    manual();
 }
 
-dropall();
+my @collect = (
+    'south', 'take fixed point', 'north',             'west',
+    'west',  'west',             'take hologram',     'east',
+    'east',  'east',             'north',             'take candy cane',
+    'west',  'take antenna',     'west',              'take shell',
+    'east',  'south',            'take whirled peas', 'north',
+    'east',  'north',            'north',             'take polygon',
+    'south', 'west',             'take fuel cell',    'west',
+    'west',
+);
+
+my @things = map { my $t = $_; $t =~ s/take //; $t } grep {/take/} @collect;
+
+$intcode->ascii_input_bulk(@collect);
+$intcode->ascii_run;
+
 try_items(@things);
 
-#my @match=('candy cane', 'fixed point', 'polygon', 'shell');
-#pickup(@match);
-#manual();
-
-sub try_items {
-    my @list = @_;
-    my @try = @list;
-    my $max = scalar @try;
-
-    for my $cnt (0 .. $max) {
-        my $c = Math::Combinatorics->new(
-            count => $max - $cnt,
-            data => \@try,
-        );
-        while (my @combo = $c->next_combination) {
-            say "COMBO $cnt ".join(", ",sort @combo);
-            dropall();
-            for my $try (@combo) {
-                input('take '.$try);
-                doit();
-            }
-            input('west');
-            doit();
-        }
-    }
-}
+# dropall();
+# my @match=('candy cane', 'fixed point', 'polygon', 'shell');
+# pickup(@match);
+# manual();
 
 sub manual {
     while (1) {
         my $in = <STDIN>;
         chomp($in);
-        input($in);
-        doit();
+        $intcode->ascii_input($in);
+    }
+}
+
+sub try_items {
+    my @list = @_;
+    my $max  = scalar @list;
+    for my $cnt ( 0 .. $max ) {
+        my $c = Math::Combinatorics->new(
+            count => $max - $cnt,
+            data  => \@list,
+        );
+        while ( my @combo = $c->next_combination ) {
+            $intcode->show_ascii_output(0);
+            say "TRY COMBO: " . join( ', ', @combo );
+            dropall();
+            $intcode->ascii_input_bulk( map { 'take ' . $_ } @combo );
+            $intcode->waiting(0);
+            $intcode->ascii_run;
+            $intcode->show_ascii_output(1);
+            $intcode->ascii_input('west');
+        }
     }
 }
 
 sub dropall {
     for my $thing (@things) {
-        input('drop '.$thing);
-        doit();
+        $intcode->ascii_input( 'drop ' . $thing );
     }
 }
+
 sub pickup {
     my @stuff = @_;
     for my $thing (@stuff) {
-        input('take '.$thing);
-        doit();
-    }
-}
-
-sub input {
-    my $in = shift;
-    $intcode->input([ map { ord($_)} split(//,$in), "\n" ]);
-    $intcode->waiting(0);
-}
-
-sub doit {
-    while (!$intcode->waiting) {
-        $intcode->runit;
-        my $o = $intcode->output;
-        if ($o == 10) {
-            $newline++;
-            exit if $newline > 10;
-        }
-        else {
-            $newline = 0;
-        }
-        print chr($o);
+        $intcode->ascii_input( 'take ' . $thing );
     }
 }
 
