f638244c68bf6b1d2213f3f93c100ddf3f534c32
Thomas Klausner <domm@plix.at>
2019-12-27 23:08:48 +0100

    use new Intcode helpers for day 23 part 1

--- 23_1.pl
+++ 23_1.pl
@@ -7,32 +7,23 @@ use Intcode;
 my %nodes;
 for my $id ( 0 .. 49 ) {
     my $computer = $nodes{$id} = Intcode->from_file( $ARGV[0] );
-    $computer->input( [ $id, -1 ] );
-    $computer->runit;
+    $computer->input_and_run($id, -1);
 }
 
 while (1) {
     for my $id ( 0 .. 49 ) {
         my $c = $nodes{$id};
-        my @output;
-        while ( !$c->waiting ) {
-            push( @output, $c->output );
-            $c->runit;
-        }
-        $c->input( [-1] );
-        $c->waiting(0);
-        $c->runit;
+        my @output = $c->collect_output;
+
+        $c->input_and_run(-1);
 
         while ( my ( $target, $x, $y ) = splice( @output, 0, 3 ) ) {
             if ( $target eq '255' ) {
                 say "to 255: $y";
                 exit;
             }
-
             #say "from $id to $target: $x, $y";
-            $nodes{$target}->input( [ $x, $y ] );
-            $nodes{$target}->waiting(0);
-            $nodes{$target}->runit;
+            $nodes{$target}->input_and_run($x, $y);
         }
     }
 }
